5ac4d041c4650454fb9987f2d8791fe2d1d1d57f
sirjanabhatta06 <sirjanabhatta06@gmail.com>
2024-02-20 18:52:56 -0500

    Analysis of housing supply

--- Scripts/Project_Supply_Query.sql
+++ Scripts/Project_Supply_Query.sql
@@ -1,9 +1,9 @@
-SELECT YEAR(DATE) AS year,
+SELECT [DATE],
 		AVG(CSUSHPISA) AS Average_home_price_index,
 		AVG(MSACSR) AS Average_monthly_new_house_supply,
 		AVG(PERMIT) as Average_new_priv_units_permits,
 		AVG(TLRESCONS) as Average_res_construct_spending,
 		AVG(EVACANTUSQ176N) as Average_vacant_units
 FROM supply_table
-GROUP BY YEAR(DATE)
-ORDER BY YEAR(DATE);
+GROUP BY [DATE]
+ORDER BY [DATE]
